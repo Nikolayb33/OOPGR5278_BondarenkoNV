6d9a0216e1d651190b71300d5cb662cf4308b582
Nikolayb33 <nikolayb33@gmail.com>
2024-01-30 00:49:56 +0300

    add task 4 for hw_6

--- seminar_6/hw_6/src/App.java
+++ seminar_6/hw_6/src/App.java
@@ -176,15 +176,16 @@ public class App {
 
         double volume();
     }
-/*
- *  чтобы убрать объем у круга
- * надо убрать интерфейс Shape и создать другой интерфейс для фигуры
- */
+    /*
+     * чтобы убрать объем у круга
+     * надо убрать интерфейс Shape и создать другой интерфейс для фигуры
+     */
 
     public interface Figure {
         double area();
 
     }
+
     public class Circle implements Figure {
         private double radius;
 
@@ -215,14 +216,55 @@ public class App {
             return edge * edge * edge;
         }
     }
-//     •Подсказка:
+    // •Подсказка:
 
-//     круг не
-//     объемная фигура
-//     и этому
-//     классу не
-//     нужен метод
+    // круг не
+    // объемная фигура
+    // и этому
+    // классу не
+    // нужен метод
 
-//     volume().
-// •
+    // volume().
+    // •
+
+    // 4) Переписать код в соответствии с Liskov Substitution Principle:
+    public class Rectangle {
+        private int width;
+        private int height;
+
+        public void setWidth(int width) {
+            this.width = width;
+        }
+
+        public void setHeight(int height) {
+            this.height = height;
+        }
+
+        public int area() {
+            return this.width * this.height;
+        }
+    }
+    /*
+     * класс делаем таким же как и его родителя (чтобы можно было использовать,
+     *  где можно использовать родителя)
+     */
+    public class Square extends Rectangle {
+        @Override
+        public void setWidth(int width) {
+            super.width = width;
+            //super.height = width;
+        }
+
+        @Override
+        public void setHeight(int height) {
+            //super.width = height;
+            super.height = height;
+        }
+
+        @Override
+        public int area() {
+            return super.width * super.height;
+        }
+    }
+
 }
